2ac9fdb7b8b48558145c05fdd189117f1d79976f
Nikolay Bambov <65560163+bambovn@users.noreply.github.com>
2021-09-28 20:40:17 +0300

    push

--- javascript/Advanced/12. Exercise DOM Manipulations and Events/01. Sections/script.js
+++ javascript/Advanced/12. Exercise DOM Manipulations and Events/01. Sections/script.js
@@ -1,3 +1,29 @@
 function create(words) {
-   console.log('TODO:...');
+
+    // create <div>
+    // create <p>
+    //set p content
+    // configure <p> to behidden (display: none)
+    // add <p> to <div>
+    //addd <div> to page
+    // configure <div> eventListener
+
+    const content = document.getElementById('content')
+
+    for (let word of words) {
+        const div = document.createElement('div')
+        const para = document.createElement('p')
+        para.textContent = word
+        para.style.display = 'none'
+        div.appendChild(para)
+
+        div.addEventListener('click', reveal)
+
+        content.appendChild(div)
+    }
+
+    function reveal(e) {
+        e.currentTarget.children[0].style.display = ''
+
+    }
 }
--- javascript/Advanced/12. Exercise DOM Manipulations and Events/03. Locked-Profile/app.js
+++ javascript/Advanced/12. Exercise DOM Manipulations and Events/03. Locked-Profile/app.js
@@ -1,3 +1,29 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+
+    Array.from(document.querySelectorAll('.profile button')).forEach(b => b.addEventListener('click', onToggle));
+
+    function onToggle(e) {
+
+        const profile = e.target.parentElement
+        const isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked
+        const infoDif = Array
+            .from(e.target.parentElement.querySelectorAll('div'))
+            .find(d => d.id.includes('HiddenFields'))
+
+        if (isActive) {
+            const infoDif = profile.querySelector('div')
+
+
+            if (e.target.textContent == 'Show more') {
+                infoDif.style.display = 'block'
+                e.target.textContent = 'Hide it'
+            } else {
+                infoDif.style.display = ''
+                e.target.textContent = 'Show more'
+
+            }
+        }
+
+    }
 }
