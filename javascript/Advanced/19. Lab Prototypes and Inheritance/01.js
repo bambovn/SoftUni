d4e289cc58639a4579392d644fc193e272ea7f52
Nikolay Bambov <65560163+bambovn@users.noreply.github.com>
2021-10-11 21:36:54 +0300

    lab 11.10.2021

---
+++ javascript/Advanced/19. Lab Prototypes and Inheritance/01.js
@@ -0,0 +1,27 @@
+
+
+function createPerson(firstName, lastName) {
+    const result = {
+        firstName,
+        lastName,
+        fullName: ''
+    };
+
+    Object.defineProperties(result, 'fullName', {
+        get() {
+            return `${this.firstName} ${this.lastName}`
+        },
+        set(value) {
+            const [first, last] = value.split(' ');
+            if (first != undefined && last != undefined) {
+                this.firstName = first;
+                this.lastName = last;
+            }
+        },
+        configurable: true,
+        enumerable: true
+
+    });
+    return result;
+}
+
